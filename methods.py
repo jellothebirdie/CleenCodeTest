3012db165621bf5248888655311c64958eefc0d1
Dianna <dg109@wellesley.edu>
2025-11-22 19:06:48 -0500

    indent

--- methods.py
+++ methods.py
@@ -65,9 +65,11 @@ def (nums: List[int], target: int) -> List[int]:
                 return [i, j]""",
                         "description":   
 """
-Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+Given an array of integers nums and an integer target, return the 
+indices i and j such that nums[i] + nums[j] == target and i != j.
 
-You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+You may assume that every input has exactly one pair of indices 
+i and j that satisfy the condition.
 
 Return the answer with the smaller index first.
 
